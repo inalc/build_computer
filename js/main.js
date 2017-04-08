0428bfe5d53104cdfc93a11932d9cc76d06d2a18
Ina Alcantara <inaalcantara1@gmail.com>
2017-04-07 22:09:19 -0400

    Added float prop and speech auto scroll

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
   makeCat();
-
+  makeDialog();
 });
 
 function makeCat() {
   $('.cat').append('<div class=head id=floating></div>');
+}
 
+function makeDialog() {
+  $('.dialog').append('<div class=speechbubble>Insert Dialog here</div>');
 }
