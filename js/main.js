ae2657793bb67e14b3a914ac57b6d27b1635b956
inalc <inaalcantara1@gmail.com>
2017-04-10 11:27:35 -0400

    Added bootstrap and jquery ui effects

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
 $(document).ready(function() {
   makeCat();
   makeDialog();
+
+
+  $('.cat').on('click', function() {
+  	$(this).effect('pulsate', 3);
+
+  });
 });
 
 function makeCat() {
   $('.cat').append('<div class=head id=floating></div>');
+
 }
+
+
 
 function makeDialog() {
   $('.dialog').append('<div class=speechbubble>Insert Dialog here</div>');
