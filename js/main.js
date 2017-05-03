39ee959249e0df8093f2cae1867c150d858685b9
inalc <inaalcantara1@gmail.com>
2017-05-03 11:35:19 -0400

    Fixed divs alignments, added situation logic

--- js/main.js
+++ js/main.js
@@ -1,21 +1,39 @@
 $(document).ready(function() {
-  makeCat();
-  makeDialog();
+  showCat();
+  //makeDialog();
 
 
-  $('.cat').on('click', function() {
-  	$(this).effect('pulsate', 3);
+  var $allSituations = $('.situation');
+  var $defaultSituation = $('.default-situation');
+  var $allOptions = $('.option');
 
+  $allSituations.hide();
+  $allOptions.hide();
+  $defaultSituation.fadeIn(2000);
+  $allOptions.delay(4000).fadeIn(2000);
+
+  $('.option').on('click', function() {
+  	var targetName = $(this).data('situation-target');
+  	var $target = $('.situation[data-situation="' + targetName + '"]')
+
+  	if($target.length == 1) {
+  		$allSituations.hide();
+  		$target.fadeIn(3000);
+  	}
   });
+
+  //$('.cat').on('click', function() {
+
+  //});
 });
 
-function makeCat() {
-  $('.cat').append('<div class=head id=floating></div>');
+function showCat() {
+  $('.cat').append('<img src="images/cat.png"  id="floating" alt="cat" width="100px" height="100px"></div>');
 
 }
-
-
+//<div class=head id=floating>
 
 function makeDialog() {
-  $('.dialog').append('<div class=speechbubble>Insert Dialog here</div>');
+  $('.situation-text').append('<div class=speechbubble></div>');
+
 }
